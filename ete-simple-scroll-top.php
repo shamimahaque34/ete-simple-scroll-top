e654b7bd018921f891a8103c1dda6b6ea2294640
Shamima Haque <haqueshamima34@gmail.com>
2024-08-07 14:47:42 +0600

    first commit

---
+++ ete-simple-scroll-top.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Ete Simple Scroll Top
+ * Plugin URI:        https://wordpress.com/plugins/ete-simple-scroll-top/
+ * Description:       Ete Simple Scroll Top plugin will help you to enable Back    to Top button to your Wordpress website.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      8.2
+ * Author:            Shamima Haque
+ * Author URI:        https://shamima.xyz/
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.en.html
+ * Update URI:        https://github.com/shamimahaque34
+ * Text Domain:       esst
+ */
+
+?>
